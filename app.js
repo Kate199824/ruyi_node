da9e94fab0a3ba48a819bc4c1eaece358cdb2727
靳炳淑 <jinbingshu_nju@qq.com>
2019-05-05 14:13:43 +0800

    change port

--- app.js
+++ app.js
@@ -75,5 +75,5 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
-console.log('server is listening on port 9000');
+app.listen(80);
+console.log('server is listening on port 80');
